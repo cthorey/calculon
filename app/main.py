02f4376e5a190a750a698354e71affbe062a2f86
cthorey <cthorey@xihelm.com>
2020-12-05 10:07:14 +0100

    [FIX] strip spaces on each end..

--- app/main.py
+++ app/main.py
@@ -10,11 +10,15 @@ app = FastAPI()
 
 @app.get("/calculate_prefix/{expression}")
 def calculate_prefix(expression: str):
+    # strip spaces on each end
+    expression = expression.strip()
     result = calculator.calculate_prefix(expression)
     return {'result': result}
 
 
 @app.get("/calculate_infix/{expression}")
 def calculate_infix(expression: str):
+    # strip spaces on each end
+    expression = expression.strip()
     result = calculator.calculate_infix(expression)
     return {'result': result}
