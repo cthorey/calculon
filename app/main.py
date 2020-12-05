90e74777b238f82c3467f2209a14e6bf5090602f
cthorey <cthorey@xihelm.com>
2020-12-05 10:54:30 +0100

    [FIX] change to POST --> GET can't handle /

--- app/main.py
+++ app/main.py
@@ -4,21 +4,28 @@ This is using FastAPI.
 """
 from calculon import calculator
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/calculate_prefix/{expression}")
-def calculate_prefix(expression: str):
-    # strip spaces on each end
-    expression = expression.strip()
-    result = calculator.calculate_prefix(expression)
+class Body(BaseModel):
+    # expression to be computed
+    expression: str
+
+    @property
+    def notation(self):
+        # small clean up to prevent error if whitespace on each end
+        return self.expression.strip()
+
+
+@app.post("/calculate_prefix/")
+def calculate_prefix(expression: Body):
+    result = calculator.calculate_prefix(expression.notation)
     return {'result': result}
 
 
-@app.get("/calculate_infix/{expression}")
-def calculate_infix(expression: str):
-    # strip spaces on each end
-    expression = expression.strip()
-    result = calculator.calculate_infix(expression)
+@app.post("/calculate_infix")
+def calculate_infix(expression: Body):
+    result = calculator.calculate_infix(expression.notation)
     return {'result': result}
