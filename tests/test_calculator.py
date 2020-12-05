069ec414e922cd59cfc9ecbf444b216196691a35
cthorey <cthorey@xihelm.com>
2020-12-05 09:55:17 +0100

    [FIX] test for infix calculation

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -20,6 +20,14 @@ PREFIX = {
     '- + 1 2 + * 3 2 5': -8
 }
 
+# infix test cases
+INFIX = {
+    '( 1 + 2 )': 3,
+    '( 1 + ( 2 * 3 ) )': 7,
+    '( ( 1 * 2 ) + 3 )': 5,
+    '( ( ( 1 + 1 ) / 10 ) - ( 1 * 2 ) )': -2
+}
+
 
 def test_prefix_calculator():
     print(MSG.format(name='prefix calculator'))
@@ -30,3 +38,13 @@ def test_prefix_calculator():
         assert result == value, "Fail on \"{}\". Expected :{} / Returned: {}".format(
             expression, value, result)
     print('Sucessfully ran prefix test \n')
+
+
+def test_infix_calculator():
+    print(MSG.format(name='infix calculator'))
+    for expression, value in INFIX.items():
+        print('Checking {} : {}'.format(expression, value))
+        result = calculator.calculate_infix(expression)
+        assert result == value, "Fail on \"{}\". Expected :{} / Returned: {}".format(
+            expression, value, result)
+    print('Sucessfully ran infix test  \n')
